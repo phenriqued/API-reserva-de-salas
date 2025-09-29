8117e93f45276ef63ee07270d0c5850ba53e7806
phenriqued <112258290+phenriqued@users.noreply.github.com>
2025-09-29 11:26:53 -0300

    Initial commit

---
+++ src/main/java/com/github/phenriqued/api_reserva_salas/ReservasDeSalasApplication.java
@@ -0,0 +1,13 @@
+package com.github.phenriqued.api_reserva_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasDeSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasDeSalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/phenriqued/api_reserva_salas/ReservasDeSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phenriqued.api_reserva_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasDeSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
