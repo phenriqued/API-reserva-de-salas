fbe12babd3f5f81a4e91bfbbf8ce012171c30dd9
phenriqued <112258290+phenriqued@users.noreply.github.com>
2025-10-09 18:03:15 -0300

    adicionar refatoracao ReservaService

--- src/main/java/com/github/phenriqued/api_reserva_salas/DTOs/ReservaDTO/CriarDadosReserva.java
+++ src/main/java/com/github/phenriqued/api_reserva_salas/DTOs/ReservaDTO/CriarDadosReserva.java
@@ -1,5 +1,7 @@
 package com.github.phenriqued.api_reserva_salas.DTOs.ReservaDTO;
 
+import com.github.phenriqued.api_reserva_salas.Infra.Exceptions.BusinessRuleException.BusinessRuleException;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
@@ -10,7 +12,16 @@ public record CriarDadosReserva(
         @NotNull
         Long salaId,
         @NotNull
+        @Future(message = "A data/hora da reserva deve ser no futuro")
         LocalDateTime inicioReserva,
         @NotNull
+        @Future(message = "A data/hora da reserva deve ser no futuro")
         LocalDateTime fimReserva) {
+    public CriarDadosReserva{
+        if(fimReserva.isBefore(inicioReserva)){
+            throw new BusinessRuleException("O periodo inicial deve anteceder o fim");
+        }
+    }
+
+
 }
